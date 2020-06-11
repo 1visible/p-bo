557c8a96137db5a36965d1736f25513b3d6e02c2
1visible <redx.6345@gmail.com>
2020-06-11 13:45:51 +0400

    Anti-bot verification bugfix

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
 require('dotenv').config();
-const { Client } = require('discord.js');
-const bot = new Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-bot.on('ready', () => {
-    console.log(`${bot.user.tag} est connecté`);
+client.on('ready', () => {
+    console.log(`${client.user.tag} est connecté`);
+    client.channels.cache.get('712009032273100971').messages.fetch('719894542077263933');
 });
 
-bot.on('message', message => {
+client.on('message', message => {
     if(!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
 
     const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-
 });
 
-bot.on('messageReactionAdd', (messageReaction, user) => {
+client.on('messageReactionAdd', (reaction, user) => {
+    console.log('init');
     if(user.bot) return;
-    if(messageReaction.message.channel.id === '712009032273100971') {
-        messageReaction.remove();
-        if(messageReaction.message.id === '719894542077263933' && messageReaction.emoji === '✅') {
-            const member = messageReaction.message.guild.member(user);
+    if(reaction.message.channel.id === '712009032273100971')
+        if(reaction.message.id === '719894542077263933' && reaction.emoji.name === '✅') {
+            const member = reaction.message.guild.member(user);
             member.roles.add('719887496053260328');
-            messageReaction.message.react('✅');
         }
-    }
+        else reaction.remove();
 });
 
-bot.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'lab');
     if (!channel) return;
     channel.send(`Welcome to the server, ${member}`);
 });
 
-bot.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
