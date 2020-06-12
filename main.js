e2b3fee9dc9077141f8f768fca44d644bed59420
1visible <redx.6345@gmail.com>
2020-06-12 11:39:29 +0400

    Remove role on rules decline

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} est connecté`);
-    client.channels.cache.get('712009032273100971').messages.fetch('720725662700273674');
+    console.log(`${client.user.tag} is online and ready`);
+    // Fetch and cache uncached messages from #rules channel
+    const channel = client.channels.cache.get('712009032273100971');
+    if(channel) channel.messages.fetch({limit: 1}, true);
 });
 
 client.on('message', message => {
     if(!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
-
     const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 });
@@ -17,11 +18,22 @@ client.on('message', message => {
 client.on('messageReactionAdd', (reaction, user) => {
     if(user.bot) return;
     if(reaction.message.channel.id === '712009032273100971')
-        if(reaction.message.id === '720725662700273674' && reaction.emoji.name === '✅') {
+        if(reaction.emoji.name === '✅') {
             const member = reaction.message.guild.member(user);
-            member.roles.add('719887496053260328');
+            if(!member.roles.cache.has('719887496053260328'))
+                member.roles.add('719887496053260328');
         }
         else reaction.remove();
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+    if(user.bot) return;
+    if(reaction.message.channel.id === '712009032273100971')
+        if(reaction.emoji.name === '✅') {
+            const member = reaction.message.guild.member(user);
+            if(member.roles.cache.has('719887496053260328'))
+                member.roles.remove('719887496053260328');
+        }
 });
 
 client.on('guildMemberAdd', member => {
